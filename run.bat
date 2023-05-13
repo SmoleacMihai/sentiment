a2dd00abaa17ac45317c27fd11d7dc7c1fb4f178
Mihai <mihaismoleac2002@gmail.com>
2023-05-13 14:52:29 +0300

    added run bat file to run faster the app

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-cd ./back
+@echo off
+
+REM Activate the virtual environment for the back-end
+call back\.venv\Scripts\activate
+
+REM Start the back-end
+start "Back" cmd /c "cd back && pip install -r requirements.txt && python main.py"
+
+REM Start the front-end
+start "Front" cmd /c "cd front && npm i && npm run dev"
